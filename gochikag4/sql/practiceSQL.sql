c83b55b60bcabd2b0472e4d1c2a08e505a2623bb
internousdev <YoshikazuSakamoto>
2017-03-08 11:01:18 +0900

    これですか？

---
+++ gochikag4/sql/practiceSQL.sql
@@ -0,0 +1,14 @@
+use database gochikag;
+
+insert into item values (1, 'table1', 2500, 'table1.jpg', 20, '2017-3-7 20:20:00', '2017-3-7 20:20:00', 'kagu');
+
+insert into item values (2, 'table2', 2500, 'table2.jpg', 20, 0, '2017-3-7 20:20:00', '2017-3-7 20:20:00', 'kagu');
+
+insert into user values(1, "tom", "tom@gochikagu.com", "tom", "0", "1");
+ 
+insert into user values(2, "krs", "krs@gochikagu.com", "krs", "0", "1");
+
+
+insert into purchase values(1, 1, 1, 2500, '2017-3-7 20:20:00', '2017-3-7 20:20:00');
+ 
+insert into purchase values(2, 1, 1, 2500, '2017-3-7 20:20:00', '2017-3-7 20:20:00');
