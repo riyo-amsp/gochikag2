71fca89b67c30203bfd5ab545bfda2866315fde7
internous <internous@00174045119440>
2017-03-09 15:38:50 +0900

    gochikag5

---
+++ gochikag5/sql/practiceSQL.sql
@@ -0,0 +1,10 @@
+use gochikag
+
+insert into user values(1, "tom", "tom@gochikagu.com", "tom", "0", "1");
+ 
+insert into user values(2, "krs", "krs@gochikagu.com", "krs", "0", "1");
+
+
+insert into purchase values(1, 1, 1, 2500, '2017-3-7 20:20:00', '2017-3-7 20:20:00');
+ 
+insert into purchase values(2, 1, 1, 2500, '2017-3-7 20:20:00', '2017-3-7 20:20:00');
---
+++ gochikag5/src/dto/CategoryDTO.java
@@ -0,0 +1,35 @@
+package dto;
+
+public class CategoryDTO {
+	private int itemId;
+	private String itemName;
+	private int price;
+	private String url;
+
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+
+}
