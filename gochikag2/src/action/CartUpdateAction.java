4fa83d3f1b684e6cafa6c7720b1b23639f4b0592
internousdev <internousdev@00174045145781X>
2017-03-10 17:58:44 +0900

    yahoo

--- gochikag2/src/action/CartUpdateAction.java
+++ gochikag2/src/action/CartUpdateAction.java
@@ -1,27 +1,63 @@
 package action;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 import dao.CartUpdateDAO;
 import dto.CartUpdateDTO;
 
-public class CartUpdateAction extends ActionSupport{
+public class CartUpdateAction extends ActionSupport implements SessionAware{
 
 
 	//フィールド
-	private String item_id;
-	private String item_count;
+	private String itemCount;
+	private int userId;
+	private String itemId;
+	private int price;
+	private int amount;
+	private List<CartUpdateDTO> itemList;
 	private Map<String,Object> session;
+	private int totalPrice;
 
 
 	//executeメソッド
-	public String execute(){
+	public String execute() throws SQLException{
+
+		System.out.println("tt");
 
 		CartUpdateDAO dao=new CartUpdateDAO();
 		CartUpdateDTO dto=new CartUpdateDTO();
-		int rs=dao.update(Integer.parseInt(item_id),Integer.parseInt(item_count));
+
+		System.out.println("tekito");
+
+		userId=0;
+		if(session!=null){
+			userId=(int)session.get("id");
+			System.out.println(userId);
+
+		}
+
+		System.out.println("hima");
+
+		int rs=dao.update(Integer.parseInt(itemId),Integer.parseInt(itemCount),price,amount,userId);
+
+		//System.out.println("hima");
+
+		itemList = new ArrayList<CartUpdateDTO>();
+		itemList = dao.select(userId);
+		totalPrice = 0;
+		for(CartUpdateDTO item : itemList){
+			totalPrice += item.getAmount();
+		}
+
+		//setItemList(new ArrayList<CartUpdateDTO>());
+		//setItemList(dao.select(userId));
 
 		if(rs==0){
 			return ERROR;
@@ -34,24 +70,86 @@ public class CartUpdateAction extends ActionSupport{
 	}
 
 
-	public String getItem_id() {
-		return item_id;
+	public String getItemCount() {
+		return itemCount;
 	}
 
 
-	public void setItem_id(String item_id) {
-		this.item_id = item_id;
+	public void setItemCount(String itemCount) {
+		this.itemCount = itemCount;
 	}
 
 
-	public String getItem_count() {
-		return item_count;
+	public int getUserId() {
+		return userId;
 	}
 
 
-	public void setItem_count(String item_count) {
-		this.item_count = item_count;
+	public void setUserId(int userId) {
+		this.userId = userId;
 	}
+
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+
+	public String getItemId() {
+		return itemId;
+	}
+
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+
+	public List<CartUpdateDTO> getItemList() {
+		return itemList;
+	}
+
+
+	public void setItemList(List<CartUpdateDTO> itemList) {
+		this.itemList = itemList;
+	}
+
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+
 }
 
 
