0afce652f01eba00768be2de356daf65c1190fd4
internous <internous@00174045119440>
2017-03-23 11:31:13 +0900

    tom

---
+++ gochikag_final/sql/insert.sql
@@ -0,0 +1,83 @@
+/*INSERT detail*/
+
+/*kagu_item*/
+insert into item values(1,'キャビネット',15800,'img/item/kagu/cabinet_main.jpg',10,'0','2017/03/17','2017/03/17','kagu');
+insert into item values(2,'アンティークチェスト',7800,'img/item/kagu/chest_main.jpg',10,'0','2017/03/17','2017/03/17','kagu');
+insert into item values(3,'テーブル',5800,'img/item/kagu/table_main.jpg',10,'0','2017/03/17','2017/03/17','kagu');
+insert into item values(4,'ドレッシングチェスト',15800,'img/item/kagu/dresser_main.jpg',10,'0','2017/03/17','2017/03/17','kagu');
+insert into item values(5,'ソファー',10100,'img/item/kagu/sofa_main.jpg',10,'0','2017/03/17','2017/03/17','kagu');
+insert into item values(6,'サロンチェアー',12800,'img/item/kagu/chair_main.jpg',10,'0','2017/03/17','2017/03/17','kagu');
+
+
+/*kagu_pictures*/
+insert into pictures values(1,'img/item/kagu/cabinet_sub1.jpg','img/item/kagu/cabinet_sub2.jpg','img/item/kagu/cabinet_sub3.jpg','0');
+insert into pictures values(2,'img/item/kagu/chest_sub1.jpg','img/item/kagu/chest_sub2.jpg','img/item/kagu/chest_sub3.jpg','0');
+insert into pictures values(3,'img/item/kagu/table_sub1.jpg','img/item/kagu/table_sub2.jpg','img/item/kagu/table_sub3.jpg','0');
+insert into pictures values(4,'img/item/kagu/dresser_sub1.jpg','img/item/kagu/dresser_sub2.jpg','img/item/kagu/dresser_sub3.jpg','0');
+insert into pictures values(5,'img/item/kagu/sofa_sub1.jpg','img/item/kagu/sofa_sub2.jpg','img/item/kagu/sofa_sub3.jpg','0');
+insert into pictures values(6,'img/item/kagu/chair_sub1.jpg','img/item/kagu/chair_sub2.jpg','img/item/kagu/chair_sub3.jpg','0');
+
+/*kagu detail*/
+insert into detail values(1,'特別な食器を、飾りながら収納できます。たくさん物が置けるのでリビングで実用的に使えます。アンティークの鍵が付いています。鍵を取っ手のようにして開閉してください。','You can house special dishes into this cabinet.It is very useful because you can place a lot of things.Open and close the door by using an antique key.','0');
+insert into detail values(2,'高級感あふれる雰囲気が素敵なアンティークの小さなチェストです。時計や携帯を置いたり、写真を飾ったりとディスプレイも楽しめます。','This is a small antique chest who adds a touch of class to the room. You can enjoy displaying a variety of goods (clock,pictures...etc) on the chest.','0');
+insert into detail values(3,'美しいアンティークティーテーブルです。波打つような天板のカッティングやしなやかに伸びた美しい脚がポイントです。そっと壁に付けて置くだけで、オシャレな雰囲気に。玄関や廊下など狭い場所でも使えます。','This is an antique table who has beautiful designs and slender legs. If you buy this one, you can change the atmosphere in your room and also use it in narrow places. (entrance,hallway...)','0');
+insert into detail values(4,'コンパクトなサイズのチェストの上に、可愛い三面鏡の付いたドレッシングチェストです。リビングや目立つところに積極的に飾って自慢したくなっちゃいます。','What a pretty dressing chest this is! You will want to show off if you place it in a living room.','0');
+insert into detail values(5,'クラシカルなフレンチスタイルのソファです。大人２,３人で使えます。ミニクッションが２つ付いて、掛け心地もデザインも充実した一台です。','This is a classical french sofa with two small cushions. Thanks to this one, you will relax even if you are tired.','0');
+insert into detail values(6,'女性らしい優雅なアンティークのサロンチェアです。優しいピンク色の張り地と高級感漂う芸術的な彫りの装飾・・・見ているだけで楽しめる素敵な１脚です。','What a elegant salon chair this is! Lovely pink furniture upholsteries and artistic ornaments... Umm, you can enjoy just by seeing it!','0');
+
+
+
+
+
+
+/*kaden item*/
+insert into item values(7,'コーヒーメーカー',10000,'img/item/kaden/coffee_maker.jpg',20,'0','2017-03-17-12:00','2017-03-17-12:00','kaden');
+insert into item values(8,'トースター',1500,'img/item/kaden/toaster.jpg',20,'0','2017-03-17-12:00','2017-03-17-12:00','kaden');
+insert into item values(9,'空気清浄器',2500,'img/item/kaden/air_clean.jpg',20,'0','2017-03-17-12:00','2017-03-17-12:00','kaden');
+insert into item values(10,'蓄音機',5000,'img/item/kaden/chikuonki.jpg',20,'0','2017-03-17-12:00','2017-03-17-12:00','kaden');
+insert into item values(11,'暖炉',8000,'img/item/kaden/danro.jpg',20,'0','2017-03-17-12:00','2017-03-17-12:00','kaden');
+insert into item values(12,'ライト',4000,'img/item/kaden/light.jpg',20,'0','2017-03-17-12:00','2017-03-17-12:00','kaden');
+
+/*kaden picture*/
+insert into pictures values(7,'img/item/kaden/coffee_maker2.jpg','img/item/kaden/coffee_maker3.jpg','img/item/kaden/coffee_maker4.jpg','0');
+insert into pictures values(8,'img/item/kaden/air_clean2.jpg','img/item/kaden/air_clean3.jpg','img/item/kaden/air_clean4.jpg','0');
+insert into pictures values(9,'img/item/kaden/toaster2.jpg','img/item/kaden/toaster3.jpg','img/item/kaden/toaster4.jpg','0');
+insert into pictures values(10,'img/item/kaden/chikuonki_sub1.jpg','img/item/kaden/chikuonki_sub2.jpg','img/item/kaden/chikuonki_sub3.jpg','0');
+insert into pictures values(11,'img/item/kaden/danro_sub1.jpg','img/item/kaden/danro_sub2.jpg','img/item/kaden/danro_sub3.jpg','0');
+insert into pictures values(12,'img/item/kaden/light_sub1.jpg','img/item/kaden/light_sub2.jpg','img/item/kaden/light_sub3.jpg','0');
+
+/*kaden detail*/
+insert into detail values(7,'コーヒー通を唸らせる、プロのハンドドリップにも負けない本格的な仕上がり。','Let us have some good coffee by using this high-spec machine.','0');
+insert into detail values(8,'コロンとしたフォルムがかわいいポップアップトースター','This is a pretty pop-up toaster.You can make toast very well.','0');
+insert into detail values(9,'アンドレ空気清浄機は植物のフィルターを使う為、通常の空気清浄機で使う取替フィルターが必要ありません。その独自のフォルムとシンプルな作りからどんなお部屋でに馴染み、植物がもつ浄化能力を飛躍的に高めてくれます。また8個のLED電球がインテリアライトとしても美しくお部屋を変えてくれます。','This air cleaner has a vegetable filter instead of normal one. Thanks to this function,it is possible to improve purifying capability dramatically.Besides, eight LED bulb change the atomospere of your room.','0');
+insert into detail values(10,'ふんわりやわらかいライトが照らしてくれます。','This light shine on you softly and kindly.','0');
+insert into detail values(11,'電気で動く”いまどき”な暖炉です。','This is a modern fireplace running on electricity.','0');
+insert into detail values(12,'スマホでは味わえないおしゃれな音楽を味わえます。','You can enjoy wonderful music with this phonograph.','0');
+
+
+
+
+
+/*sale item*/
+insert into item values(13,"キャビネット", 6000, "img/item/sale/cabinet_main.jpg", 20, false, "2017-03-17-12:00", "2017-03-17-12:00", "sale");
+insert into item values(14,"シャンデリア",8000, "img/item/sale/chandelier_main.jpg",20,false,"2017-03-17-12:00","2017-03-17-12:00","sale");
+insert into item values(15,"時計", 3500, "img/item/sale/clock_main.jpg", 20,false,"2017-03-17-12:00","2017-03-17-12:00","sale");
+insert into item values(16,"ソファー", 7000,"img/item/sale/sofa_main.jpg",20,false,"2017-03-17-12:00","2017-03-17-12:00","sale");
+insert into item values(17,"ゴミ箱",2000,"img/item/sale/dust_box_main.jpg",20,false,"2017-03-17-12:00","2017-03-17-12:00","sale");
+insert into item values(18,"ティータイム",2000,"img/item/sale/tea_time_main.jpg",20,false,"2017-03-17-12:00","2017-03-17-12:00","sale");
+
+/*sale picture*/
+insert into pictures values(13,'img/item/sale/cabinet_sub1.jpg','img/item/sale/cabinet_sub2.jpg','img/item/sale/cabinet_sub3.jpg', false);
+insert into pictures values(14,'img/item/sale/chandelier_sub1.jpg','img/item/sale/chandelier_sub2.jpg','img/item/sale/chandelier_sub3.jpg', false);
+insert into pictures values(15,'img/item/sale/clock_sub1.jpg','img/item/sale/clock_sub2.jpg','img/item/sale/clock_sub3.jpg',false);
+insert into pictures values(16,'img/item/sale/dust_box_sub1.jpg','img/item/sale/dust_box_sub2.jpg','img/item/sale/dust_box_sub3.jpg', false);
+insert into pictures values(17,'img/item/sale/sofa_sub1.jpg','img/item/sale/sofa_sub2.jpg','img/item/sale/sofa_sub3.jpg', false);
+insert into pictures values(18,'img/item/sale/tea_time_sub1.jpg','img/item/sale/tea_time_sub2.jpg','img/item/sale/tea_time_sub3.jpg',false);
+
+/* sale detail*/
+insert into detail values(13, 'ココアやカプチーノを入れて、素敵なティータイムをどうぞ!', 'Please have a nice tea time with this teapot.', '0');
+insert into detail values(14,'思わず体も心もぴょんぴょん♪してしまうそんな一品です。','This is an amazing sofa who makes you excited.','0');
+insert into detail values(15,'｢ポイッ」って捨ててみてください。あっ・・・でも、今日を「ポイッ」てしないでくださいね（ダメです）','When you throw away the rubbish, put it in this trash box. Oops, but don not throw your life away.You only live once.','0');
+insert into detail values(16,'ラビットハウスにもこんな時計がありそうですね。','It seems to be able to find a clock like this in a rabbit house.','0');
+insert into detail values(17,'女の子の憧れをご注文しませんか？','Will you order longings of girls?','0');
+insert into detail values(18,'かわいさだけを、プレンドしました。','We blended much loveliness.','0');
